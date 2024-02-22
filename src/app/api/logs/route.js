628fa58232552f4db6efb5a1a18d8de48d8b9293
Sanjeev <sanjeevkansyakar789@gmail.com>
2024-02-22 20:48:13 +0530

    logs

--- src/app/api/logs/route.js
+++ src/app/api/logs/route.js
@@ -2,10 +2,9 @@ import Logs from "@/models/log.model";
 import { connect } from "@/dbConfig/dbConfig";
 import { NextResponse } from "next/server";
 
-connect();
-
 export async function GET() {
     try {
+        await connect();
         const logs = await Logs.find({}).sort({ createdAt: -1 });
 
         return NextResponse.json({ logs }, { status: 201 });
